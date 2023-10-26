23ac0d5d27e227bb593d7461e14577f4b90a2393
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-10-26 11:29:37 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -12,6 +12,7 @@ module.exports = {
     once: true,
   },
   execute: async (client) => {
+    //client.application.commands.set([...client.slashes.values()]).catch(err => console.error(err))
     consoled.cyan(`${client.user.username} is online on ${client.guilds.cache.size} servers with a ping of ${client.ws.ping + "ms"}!`)
     client.user.setPresence({ activities: [{ name: presence }] });
   }
